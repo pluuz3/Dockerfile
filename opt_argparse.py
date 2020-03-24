2eff0e90f0c1352543d73124dce342fe93a74a71
pluuz3 <pluuz3@yahoo.com>
2020-03-24 00:26:21 -0700

    opt_argparse.py updated

--- opt_argparse.py
+++ opt_argparse.py
@@ -1,5 +1,5 @@
 # Author : AL
-# Usage  : python3  opt_argparse.py -i inputfile -o outputfile
+# Usage  : python3  opt_argparse.py -s source -d dest -c compose
 
 
 #!/usr/bin/python3
@@ -8,20 +8,38 @@ import argparse
 import sys, os
 
 
-def add_arg(parser, short_opt, long_opt, help_msg='Cannot be empty', require=True):
-	parser.add_argument(short_opt, long_opt, help=help_msg, required=require)
+# define parser=argparse.ArgumentParser()
+# then call this function add_arg(parser, '-s', '--source', help_msg='Source file name', require='True') for each option
+def add_arg(parser, short_opt, long_opt, help_msg='Cannot be empty', require='False'):
+	if require :
+#		print (require)
+		parser.add_argument(short_opt, long_opt, help=help_msg, required=require)
+	else:
+#		print("none require")
+		parser.add_argument(short_opt, long_opt, help=help_msg)
+
+def create_3options():
+	print ("\n")
+	parser=argparse.ArgumentParser(add_help=True)
+	add_arg(parser, '-s', '--source', help_msg='Source file name', require='True')
+	add_arg(parser, '-d', '--destination', help_msg='Destination file name', require='True')
+	add_arg(parser, '-c', '--compose', help_msg='Compose file name')
+	args = parser.parse_args()
+	return args
+
+def print_3options(args):
+	## show values ##
+	print ("Input file :  %s" % args.source  )
+	print ("Output file:  %s" % args.destination )
+	print ("Compose file: %s" % args.compose )
+	print ("\n" )
 
 
-print ("\n")
-parser=argparse.ArgumentParser()
-add_arg(parser, '-s', '--source', help_msg='Source file name', require='True')
-add_arg(parser, '-d', '--destination', help_msg='Destination file name', require='True')
-add_arg(parser, '-c', '--compose', help_msg='Compose file name', require='False')
-args = parser.parse_args()
+def main():
+	my3opts = []
+	my3opts=create_3options()
+	print_3options(my3opts)
 
 
-## show values ##
-print ("Input file : %s" % args.input  )
-print ("Output file: %s" % args.output )
-print ("Compose file: %s" % args.compose )
-print ("\n" )
+if __name__ == "__main__":
+	main()
